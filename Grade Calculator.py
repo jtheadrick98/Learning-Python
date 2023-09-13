8d688a6326f065f563f8f825c298bfba4e633da0
jtheadrick98 <104476342+jtheadrick98@users.noreply.github.com>
2023-09-13 10:34:04 -0400

    Create Grade Calculator.py

---
+++ Grade Calculator.py
@@ -0,0 +1,16 @@
+#This program calculates a grade.
+
+score = float(input("Enter a grade between 0.0 and 1.0: ")
+Try:
+    if score >= 0.9:
+        print("A")
+    elif score >= 0.8:
+        print("B")
+    elif score >= 0.7:
+        print("C")
+    elif score >= 0.6:
+        print("D")
+    else:
+        print("F")
+Except:
+    print("Bad score.")
